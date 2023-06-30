574634fdd6904e1de482da110b0bdbf1cd9b9ccc
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-06-30 21:25:06 +0300

    1

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,2 +1,22 @@
 const API_KEY =
   'live_YfJvef9rlUTtEByUgRldjWVwx689jzJfQbtsZu7HiBC3nLOT7xeKKfdBcqjsYf8v';
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+export const fetchBreeds = function () {
+  return fetch(`${BASE_URL}breeds?api-key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
+
+export const fetchCatByBreed = function (breedId) {
+  return fetch(
+    `${BASE_URL}images/search?breed_ids=${breedId}&api-key=${API_KEY}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
-live_YfJvef9rlUTtEByUgRldjWVwx689jzJfQbtsZu7HiBC3nLOT7xeKKfdBcqjsYf8v;
+import { fetchBreeds, fetchCatByBreed } from '../js/cat-api';
+const select = document.querySelector('.breed-select');
+const loading = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const cat = document.querySelector('.cat-info');
+fetchBreeds()
+  .then(date => {
+    date.forEach(element => {
+      const heading = document.createElement('option');
+      heading.value = element.id;
+      heading.textContent = element.name;
+      select.appendChild(heading);
+    });
+  })
+  .catch(err => console.log(err));
+select.addEventListener('change', clickSelect);
+function clickSelect(event) {
+  const breedId = event.target.value;
+  fetchCatByBreed(breedId)
+    .then(date => markup(date))
+    .catch(error => console.log(error));
+}
+function markup(arry) {
+  const catMurkup = arry
+    .map(breed => {
+      return `<img src="${breed.url}" alt="" width= 300>
+    <h2>${breed.name}</h2>
+    <p>${breed.description}</p>
+    <span></span>`;
+    })
+    .join('');
+  cat.innerHTML = catMurkup;
+}
